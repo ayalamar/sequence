a65feb62e594e28b49816734d3f130924173043b
Maria Ayala <mayala@research.baycrest.org>
2019-01-31 14:24:52 -0500

    added some experiment scripts

---
+++ experiments/dual explicit/myexp.py
@@ -0,0 +1,28 @@
+# block structure
+def initexp(cfg):
+
+    #LOGICAL_CONFIG
+    cfg['exp_name'] = '3'
+    cfg['num_blocks'] = 9
+    cfg ['block_names'] = ['Reach with cursor', 'Reach without cursor', 'Reach with cursor', 'Reach with cursor',
+                           'Reach without cursor', 'Reach without cursor','Reach with cursor', 'Reach without cursor', 'Reach without cursor']
+    cfg['trial_numbers'] = [60,24,360,360,12,12,24,12,12]
+    cfg['rotations'] = [0, 0, 30,30, 0,0, 30,0, 0]
+    cfg['garage_distance'] = ['far', 'far', 'far','far', 'far', 'far', 'far', 'far', 'far']
+    cfg['garage_left_right'] = ['both', 'both', 'both','both','left', 'left','both','right','right']
+    cfg['target_angles'] = [75, 105, 90]
+
+    #DISPLAY_CONFIG
+    cfg['target_distance'] = 12
+    cfg['target_radius'] = 0.0375 * 2 / 3  #relative to the height of screen
+    cfg['close_garage_distance'] = 0.1 #relative to the width of screen
+    cfg['far_garage_distance'] = 9
+    cfg['visible_home_range'] = 2
+    cfg['acquire_target_limit'] = cfg['target_radius']  #relative to the height of screen
+    cfg['speed_limit'] = 9
+    cfg['acquire_target_time'] = 50
+    cfg['acquire_garage_limit'] = 2
+    cfg['garage_size'] = 3
+
+    return cfg
+
