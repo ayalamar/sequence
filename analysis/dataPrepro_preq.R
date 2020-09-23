dbb1b2fa4cc3fb229b738967f1f035f834a19a4b
Maria Ayala <mayala@Marias-iMac.local>
2020-09-23 14:29:50 -0400

    plot splined trajectories

--- analysis/dataPrepro_preq.R
+++ analysis/dataPrepro_preq.R
@@ -1,5 +1,5 @@
 ########## FOR PREQ DATA ONLY ##########
-setwd('/Users/mayala/Desktop/preq data')
+setwd('/Users/mayala/Document/preq data')
 subject_numbers <- c(1:31) 
 tasks <- c(0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12) 
 outfile_suffix <- sprintf('ALL')
